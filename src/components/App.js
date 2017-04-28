f5fe378ad22983177660c0ef0e08445e04141f95
ahribori <ahribori@gmail.com>
2017-04-28 21:49:38 +0900

    - add ahribori oAuth

--- src/components/App.js
+++ src/components/App.js
@@ -12,14 +12,71 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			auth: false
+		}
 	}
+
+	componentDidMount() {
+		const sdk = window.AHRIBORI_AUTH_SDK;
+
+		sdk.checkToken({
+			success: () => {
+				this.setState({
+					auth: true
+				})
+			},
+			fail: () => {
+				this.setState({
+					auth: false
+				})
+			}
+		});
+
+		sdk.createLoginButton({
+			container: '#ahribori_auth',
+			size: 'large',
+			success: () => {
+				this.setState({
+					auth: true
+				})
+			},
+			logout: () => {
+				this.setState({
+					auth: false
+				})
+			}
+		})
+	}
+
 	
 	render() {
-		return (
+
+		const alarmInterface = (
 			<div>
 				<ButtonSet/>
 				<AlarmRegisterForm/>
 				<AlarmList/>
+			</div>
+		);
+
+		return (
+			<div>
+				<div className="col s12 m8 offset-m2 l6 offset-l3">
+					<div className="card-panel grey lighten-5 z-depth-1">
+						<div className="row valign-wrapper">
+							<h4 style={{
+								margin: '0 auto 0 auto',
+								fontWeight: '100'
+							}}>Raspberrypi Alarm</h4>
+						</div>
+					</div>
+				</div>
+				<div id="ahribori_auth" style={{
+					margin: '10px',
+					textAlign: 'center'
+				}}></div>
+				{ this.state.auth ? alarmInterface : '' }
 			</div>
 		);
 	}
